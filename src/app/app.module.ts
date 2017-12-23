9ce4481a355a1b038e6ff397ef7f20b0e752611c
Jason Crawford <daxapps777@gmail.com>
2017-12-23 09:00:30 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
