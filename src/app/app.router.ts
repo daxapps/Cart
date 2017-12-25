093c8cf6d931fae6a34081950f55094132350112
Jason Crawford <daxapps777@gmail.com>
2017-12-25 08:34:34 -0600

    Setup carousel

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,14 +2,19 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { AboutComponent } from "./about/about.component";
-// import { ProductsComponent } from './products/products.component';
-// import { HomeComponent } from './home/home.component';
-// import { CartComponent } from './cart/cart.component';
-// import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 export const router: Routes = [
 	{ path: "", redirectTo: "home", pathMatch: "full" },
-	{ path: "about", component: AboutComponent }
+	{ path: "about", component: AboutComponent },
+	{ path: "products", component: ProductsComponent },
+	{ path: "home", component: HomeComponent },
+	{ path: "cart", component: CartComponent },
+	{ path: "sidebar-component", component: SidebarComponent },
+	
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    jQuery('.carousel').carousel({
+      interval: 2000
+    })
   }
-
+  viewProducts() {
+    this.router.navigate(['/products']);
+  }
 }
