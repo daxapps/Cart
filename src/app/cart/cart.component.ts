85811e907469c083d46e4e11555b7da97fdd4261
Jason Crawford <daxapps777@gmail.com>
2017-12-26 22:20:08 -0600

    Enable Cart functionality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DEFAULT_INTERPOLATION_CONFIG } from '@angular/compiler/src/ml_parser/interpolation_config';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartProducts: any;
+  bill: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.initiateData();
   }
 
+  initiateData() {
+    let data = localStorage.getItem('cart');
+    if(data !== null){
+      this.cartProducts = JSON.parse(data);
+      this.bill = 0;
+      for(let i in this.cartProducts){
+        this.cartProducts[i]["qt"] = 1;
+        this.bill = this.bill + this.cartProducts[i].price * this.cartProducts[i].qt;
+      }
+    } else {
+      this.cartProducts = [];
+    }
+  }
+
+  updateTotal() {
+    this.bill = 0;
+    for(let i in this.cartProducts){
+      this.bill = this.bill + this.cartProducts[i].price * this.cartProducts[i].qt;
+    }
+  }
+
+  removeItem(id) {
+    this.cartProducts.splice(id, 1);
+    if(this.cartProducts.length){
+      localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+    } else { 
+      localStorage.setItem('cart', null);
+    }
+  }
+
+  payBill() {
+    if(this.cartProducts.length){
+      localStorage.removeItem('cart');
+      this.initiateData();
+      alert("Your bill is: " + this.bill);
+    } else {
+      alert("No items in cart");
+    }
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -93,7 +93,7 @@ export class ProductsComponent implements OnInit {
     let product = this.products[index];
     let cartData = [];
     let data = localStorage.getItem('cart');
-    if(data!==null){
+    if(data !== null){
       cartData = JSON.parse(data);
     }
     cartData.push(product);
